63c0e35b01b06dabdf8c7d2a146eafaffbf1c1a8
Evan Chisholm <elchisholm248@gmail.com>
2024-07-02 22:04:18 -0400

    created simulator

--- src/simulator.ts
+++ src/simulator.ts
@@ -1,2 +1,18 @@
 import Boid from "./boid";
-import { Vec2d, getAvgVec } from "./vec";
+import { Vec2d } from "./vec";
+
+export class Simulator {
+    bounds: Vec2d;
+    boids: Boid[];
+
+    constructor(n: number, bounds: Vec2d) {
+        this.boids = Array.from({ length: n }, () => new Boid());
+        this.bounds = bounds;
+    }
+
+    update() {
+        for (const boid of this.boids) {
+            boid.update(this.boids);
+        }
+    }
+}
