58ee5d728a7b20636dcebd63d20c25fd78b6bcc4
Evan Chisholm <elchisholm248@gmail.com>
2024-07-03 22:05:53 -0400

    wrote a little render method, not sure if it works because im in hospital and too lazy to test

--- src/simulator.ts
+++ src/simulator.ts
@@ -15,4 +15,16 @@ export class Simulator {
             boid.update(this.boids);
         }
     }
+
+    render(ctx: CanvasRenderingContext2D) {
+        ctx.clearRect(0, 0, this.bounds.x, this.bounds.y);
+        for (const boid of this.boids) {
+            // render the boid
+            ctx.beginPath();
+            ctx.arc(boid.pos.x, boid.pos.y, 5, 0, 2 * Math.PI);
+            ctx.fillStyle = "black";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
 }
